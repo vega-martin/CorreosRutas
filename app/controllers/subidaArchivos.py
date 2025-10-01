084249537791eb674a8d1d8e352a89776b643387
vega-martin <vega.martin@edu.uah.es>
2025-10-01 17:44:04 +0000

    Avances subidaArchivos

--- app/controllers/subidaArchivos.py
+++ app/controllers/subidaArchivos.py
@@ -1,22 +1,41 @@
 from flask import Blueprint, request, current_app, redirect, url_for, session
 from werkzeug.utils import secure_filename
+import pandas as pd
 import os
 
 subidaArchivos_bp = Blueprint('subidaArchivos', __name__, template_folder='templates')
+
 
 def extension_valida(nombre):
     ext_permitidas = current_app.config['ALLOWED_EXTENSIONS']
     return '.' in nombre and nombre.rsplit('.', 1)[1].lower() in ext_permitidas
 
+#def fichero_valido(ruta, fichero):
+#    match fichero:
+#        case "A":
+            # TODO:
+#        case "B":
+            # TODO:
+#        case "C":
+            # TODO:
+#        case _:
+#            return False
+
+
+
 @subidaArchivos_bp.route('/subirArchivoA', methods=['POST'])
 def subir_archivo_A():
     f = request.files['FicheroA']
+    print(secure_filename(f.filename))
     if f and extension_valida(f):
         data_filename = secure_filename(f.filename)
         f.save(os.path.join(current_app.config['UPLOAD_FOLDER'], data_filename))
         session['uploaded_data_A_file_path'] = os.path.join(current_app.config['UPLOAD_FOLDER'], data_filename)
+        print(session['uploaded_data_A_file_path'])
+
         return redirect(url_for('main.inicio'))
     return 'Extensión no permitida. Asegurese de que el archivo tiene extensión CSV, XLS o XLSX', 400
+
 
 @subidaArchivos_bp.route('/subirArchivoB', methods=['POST'])
 def subir_archivo_B():
@@ -27,6 +46,7 @@ def subir_archivo_B():
         session['uploaded_data_B_file_path'] = os.path.join(current_app.config['UPLOAD_FOLDER'], data_filename)
         return redirect(url_for('main.inicio'))
     return 'Extensión no permitida. Asegurese de que el archivo tiene extensión CSV, XLS o XLSX', 400
+
 
 @subidaArchivos_bp.route('/subirArchivoC', methods=['POST'])
 def subir_archivo_C():
