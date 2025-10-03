4672bd3fabc500c9daf2676e516e81dee0991023
vega-martin <vega.martin@edu.uah.es>
2025-10-03 11:15:03 +0200

    Agregar alertas a la subida de archivos

--- app/controllers/fileUpload.py
+++ app/controllers/fileUpload.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, current_app, redirect, url_for, session
+from flask import Blueprint, request, current_app, redirect, url_for, session, jsonify, flash
 from werkzeug.utils import secure_filename
 import pandas as pd
 import os
@@ -11,64 +11,88 @@ def valid_extension(name):
     return '.' in name and name.rsplit('.', 1)[1].lower() in valid_ext
 
 
-# FIXME: hacer que lance alerta
-@fileUpload_bp.route('/validateFile')
-def valid_file(path, file):
-    df = pd.read_csv(path, delimiter=';', low_memory=False)
-    match file:
-        case "A":
-            required_columns = {'fec_lectura_medicion', 'longitud_wgs84_gd', 'latitud_wgs84_gd', 'cod_inv_pda'}
-            if not required_columns.issubset(set(df.columns)):
-                df = None
-                return 'El fichero CSV debe contener las columnas:\nfec_lectura_medicion, longitud_wgs84_gd, latitud_wgs84_gd, cod_inv_pda', 400
-        case "B":
-            required_columns = {'Num Inv', 'Fec Actividad', 'Seg Transcurrido'}
-            if not required_columns.issubset(set(df.columns)):
-                df = None
-                return 'El fichero CSV debe contener las columnas:\nNum Inv, Fec Actividad, Seg Transcurrido', 400
-        case "C":
-            required_columns = {'COD_SECCION', 'INSTANTE', 'LONGITUD', 'LATITUD'}
-            if not required_columns.issubset(set(df.columns)):
-                df = None
-                return 'El fichero CSV debe contener las columnas:\nCOD_SECCION, INSTANTE, LONGITUD, LATITUD', 400
-        case _:
-            return 'El fichero CSV no cumple ningún requisito de columnas', 400
+@fileUpload_bp.route('/validateFile', methods=['POST'])
+def valid_file(path, file_type):
+    try:
+        df = pd.read_csv(path, delimiter=';', low_memory=False)
+    except Exception as e:
+        return jsonify({'error': f'Error al leer el archivo: {str(e)}'}), 500
+
+    required_columns_map = {
+        "A": {'fec_lectura_medicion', 'longitud_wgs84_gd', 'latitud_wgs84_gd', 'cod_inv_pda'},
+        "B": {'Num Inv', 'Fec Actividad', 'Seg Transcurrido'},
+        "C": {'COD_SECCION', 'INSTANTE', 'LONGITUD', 'LATITUD'}
+    }
+
+    required_columns = required_columns_map.get(file_type)
+
+    if not required_columns:
+        return jsonify({'error': f'El archivo no cumple con los criterios'}), 400
+    
+    if not required_columns.issubset(df.columns):
+        return jsonify({'error': f'El fichero CSV debe contener las columnas: {", ".join(required_columns)}'}), 400
+    
+    return jsonify({'message': 'Archivo válido'}), 200
 
 
-# TODO: cabiar el error para que salga una alerta
 @fileUpload_bp.route('/fileUploadA', methods=['POST'])
 def upload_file_A():
     f = request.files['fileA']
     data_filename = secure_filename(f.filename)
+
     if f and valid_extension(data_filename):
-        f.save(os.path.join(current_app.config['UPLOAD_FOLDER'], data_filename))
-        session['uploaded_data_A_file_path'] = os.path.join(current_app.config['UPLOAD_FOLDER'], data_filename)
-        valid_file(session['uploaded_data_A_file_path'], "A")
+        save_path = os.path.join(current_app.config['UPLOAD_FOLDER'], data_filename)
+        f.save(save_path)
+        session['uploaded_data_A_file_path'] = save_path
+
+        response = valid_file(session['uploaded_data_A_file_path'], "A")
+        if response[1] != 200:
+            os.remove(save_path)
+            flash(response[0].json['error'], 'error')
+
         return redirect(url_for('main.root'))
-    return 'Extensión no permitida. Asegurese de que el archivo tiene extensión CSV, XLS o XLSX', 400
+    
+    flash('Extensión no permitida. Asegurese de que el archivo tiene extensión CSV, XLS o XLSX', 'error')
+    return redirect(url_for('main.root'))
 
 
-# TODO: cabiar el error para que salga una alerta
 @fileUpload_bp.route('/fileUploadB', methods=['POST'])
 def upload_file_B():
     f = request.files['fileB']
     data_filename = secure_filename(f.filename)
+
     if f and valid_extension(data_filename):
-        f.save(os.path.join(current_app.config['UPLOAD_FOLDER'], data_filename))
-        session['uploaded_data_B_file_path'] = os.path.join(current_app.config['UPLOAD_FOLDER'], data_filename)
-        valid_file(session['uploaded_data_B_file_path'], "B")
+        save_path = os.path.join(current_app.config['UPLOAD_FOLDER'], data_filename)
+        f.save(save_path)
+        session['uploaded_data_B_file_path'] = save_path
+
+        response = valid_file(session['uploaded_data_B_file_path'], "B")
+        if response[1] != 200:
+            os.remove(save_path)
+            flash(response[0].json['error'], 'error')
+        
         return redirect(url_for('main.root'))
-    return 'Extensión no permitida. Asegurese de que el archivo tiene extensión CSV, XLS o XLSX', 400
+    
+    flash('Extensión no permitida. Asegurese de que el archivo tiene extensión CSV, XLS o XLSX', 'error')
+    return redirect(url_for('main.root'))
 
 
-# TODO: cabiar el error para que salga una alerta
 @fileUpload_bp.route('/fileUploadC', methods=['POST'])
 def upload_file_C():
     f = request.files['fileC']
     data_filename = secure_filename(f.filename)
+
     if f and valid_extension(data_filename):
-        f.save(os.path.join(current_app.config['UPLOAD_FOLDER'], data_filename))
-        session['uploaded_data_C_file_path'] = os.path.join(current_app.config['UPLOAD_FOLDER'], data_filename)
-        valid_file(session['uploaded_data_C_file_path'], "C")
+        save_path = os.path.join(current_app.config['UPLOAD_FOLDER'], data_filename)
+        f.save(save_path)
+        session['uploaded_data_C_file_path'] = save_path
+
+        response = valid_file(session['uploaded_data_C_file_path'], "C")
+        if response[1] != 200:
+            os.remove(save_path)
+            flash(response[0].json['error'], 'error')
+        
         return redirect(url_for('main.root'))
-    return 'Extensión no permitida. Asegurese de que el archivo tiene extensión CSV, XLS o XLSX', 400
+    
+    flash('Extensión no permitida. Asegurese de que el archivo tiene extensión CSV, XLS o XLSX', 'error')
+    return redirect(url_for('main.root'))
