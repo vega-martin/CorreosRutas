1f2aac2385245f304a649f9f7b12949404003fbd
vega-martin <vega.martin@edu.uah.es>
2025-10-30 18:17:55 +0100

    Diseñar algortimo de unificación de ficheros

--- Backend/app/api/api.py
+++ Backend/app/api/api.py
@@ -25,7 +25,7 @@ def upload_file():
     f.save(save_path)
 
     rename_file_columns(save_path, type)
-    
+
     if not os.path.exists(save_path):
         current_app.logger.error(f"Error: el archivo no se guardó en {save_path}")
         return jsonify({'error': f'El archivo no se guardó correctamente'}), 400
@@ -33,3 +33,32 @@ def upload_file():
     current_app.logger.info(f"Archivo guardado correctamente en {save_path}")
     
     return jsonify({'error': f'Archivo guardado correctamente'}), 200
+
+
+@api_bp.route('/unifyFiles', methods=['POST'])
+def unifyFiles():
+    # TODO: la lista de abajo (en principio se ejecutará en orden)
+    # 1 - Poner fecha_hora en el mismo formato en los 3 ficheros
+    # 2 - Crear 2 columnas a partir de fecha_hora (la original se puede dejar): fecha, hora
+    # 3 - Eliminar duplicados
+    # 4 - Ordenar y dividir por PDA -> Fichero_A_PDA01, Fichero_B_PDA01, Fichero_C_PDA01
+    # 5 - Ordenar y dividir por fecha -> Fichero_A_PDA01_2025-05-29 se junta con Fichero_B_PDA01_2025-05-29 y Fichero_C_PDA01_2025-05-29
+    # 6 - Ordenar por hora
+    # 7 - Crear Fichero_D = unificar ficheros B y C (PDA;fecha_hora;fecha;hora;longitud;latitud;segundosTranscurridos)
+    #   a - Fijar umbral de tiempo
+    #   b - Tomar un fichero como referencia (va a ser el B por que lo digo yo)
+    #   c - Establecer un "puntero" al principio de ambos ficheros (va a ser el indice de un for seguramente)
+    #   c - Comparar hora (segundos) del registro:
+    #       - Si dentro del umbral agregar registro a Fichero_D_PDA01_2025-05-29
+    #       - Si fuera del umbral
+    #           + guardar diferencia temporal
+    #           + comparar con diferencia temporal del siguiente registro en el Fichero_C
+    #               ~ Si disminuye la dif. temp. se elimina el registro actual del Fichero_C (fichero observado) y repetir g
+    #               ~ Si aumenta la dif. temp. se elimina el registro actual del Fichero_B (fichero referencia) y repetir g
+    #               Agregar estos registros a un fichero a parte especificado que no se ha encotrado su registro correspondiente
+    # 8 - Crear Fichero_F = unificar ficheros A y D (PDA;fecha_hora;fecha;hora;longitud;latitud;segundosTranscurridos;esParada)
+    #   a - Se crea una columna (booleana) para especificar si es parada (fichero D) o no (fichero A)
+    #   b - Se intercalan los registros en función de la hora
+    #       - Si las horas coinciden (que no debería) no quedamos con la parada (fichero D)
+    
+    return jsonify({'error': f'Archivos unificados correctamente'}), 200
